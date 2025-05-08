dc17456d86682bd09300b6db1e8e826a4db8c710
MAROUAN17 <marinosaglagal25@gmail.com>
2025-05-08 17:28:11 +0100

    adding output box

---
+++ src/components/OutputBox.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+function OutputBox() {
+    return (
+        <Box w="50%"> 
+            <Text fontWeight="semibold" textStyle="md">fen</Text>
+        </Box>
+    )
+}
+
+export default OutputBox;
--- src/components/codeEditorBox.jsx
+++ src/components/codeEditorBox.jsx
@@ -1,7 +1,8 @@
 import { useRef, useEffect, useState } from 'react';
 import Editor from '@monaco-editor/react'
-import { Box } from "@chakra-ui/react"
+import { Box, HStack, Text } from "@chakra-ui/react"
 import LanguageSelector from "./LanguageSelector.jsx"
+import OutputBox from './OutputBox.jsx';
 
 function CodeEditorBox() {
     const editoRef = useRef();
@@ -14,24 +15,29 @@ function CodeEditorBox() {
         setContent(newContent);
     }
     return (
-        <>
-            <LanguageSelector language={selectedLang} changeLanguage={changeLanguage} />
-            <Box minH="100%" bg="#0f0a19" color="gray.500" px={6} py={8}>
-                <Editor
-                    height="75vh"
-                    width="75vh"
-                    theme='vs-dark'
-                    language={selectedLang}
-                    defaultValue="start some code..."
-                    onMount={(editor) => {
-                        editoRef.current = editor;
-                        editoRef.current.focus();
-                    }}
-                    value={content}
-                    onChange={(content) => handleChange(content)}
-                />
-            </Box>
-        </>
+        <Box>
+            <HStack spacing={4}>
+                <Box w="50%">
+                    <LanguageSelector language={selectedLang} changeLanguage={changeLanguage} />
+                    <Box minH="100%" bg="#0f0a19" color="gray.500" px={6} py={8}>
+                        <Editor
+                            height="75vh"
+                            width="50vh"
+                            theme='vs-dark'
+                            language={selectedLang}
+                            defaultValue="start some code..."
+                            onMount={(editor) => {
+                                editoRef.current = editor;
+                                editoRef.current.focus();
+                            }}
+                            value={content}
+                            onChange={(content) => handleChange(content)}
+                        />
+                    </Box>
+                </Box>
+                <OutputBox />
+            </HStack>
+        </Box>
     )
 }
 
